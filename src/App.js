bbccaeda061e6113f3fbc33997fca8d80091dbc0
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-03-27 22:41:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import components
+import Card from "./components/Card";
+
+const App = () => {
+    return (
+        <div className="w-full bg-[#000000] h-screen flex items-center justify-center">
+            <Card />
+        </div>
+    );
+};
+
+export default App;
